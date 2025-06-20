630198c48a60d5cd283f944242cb43ef2aba9b0e
Supreet37 <supreetmohapatra06@gmail.com>
2025-06-20 19:54:22 +0530

    New Commit

---
+++ form-handling-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Uncontrolled from "./Components/Uncontrolled";
+import Controlled from "./Components/Controlled";
+import "./App.css"
+
+const App = () => {
+  return(
+    <div>
+      <Controlled/>
+      
+      {/* <Uncontrolled/> */}
+    </div>
+  )
+}
+
+export default App;
